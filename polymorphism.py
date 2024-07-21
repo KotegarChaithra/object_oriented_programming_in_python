97cba20bc71923fc542b1f1a4935667f24d4e8ac
KotegarChaithra <155987898+KotegarChaithra@users.noreply.github.com>
2024-07-21 17:32:51 +0530

    9th file updated

--- polymorphism.py
+++ polymorphism.py
@@ -1,134 +1,134 @@
 # # Duck typing:
 
-# class VSCode:
+class VSCode:
 
-#     def execute(self):
-#         print("Compiling")
-#         print("Running")
+    def execute(self):
+        print("Compiling")
+        print("Running")
 
-# class MyEditor:
+class MyEditor:
 
-#     def execute(self):
-#         print("Spell chack")
-#         print("Convention check")
-#         print("Compiling")
-#         print("Running")
+    def execute(self):
+        print("Spell chack")
+        print("Convention check")
+        print("Compiling")
+        print("Running")
 
-# class Laptop:
+class Laptop:
 
-#     def code(self,ide):
-#         ide.execute()
+    def code(self,ide):
+        ide.execute()
 
-# # ide=VSCode()
-# ide = MyEditor()
+# ide=VSCode()
+ide = MyEditor()
 
-# lap1=Laptop()
-# lap1.code(ide)
+lap1=Laptop()
+lap1.code(ide)
+
+output:
+Spell chack
+Convention check
+Compiling
+Running
+
+---------------------------------------------------------------------------------------------------
+
+Operator overloading:
+
+a = 5
+b = 'World'
+print(a+b)
+
+#error:
+Traceback (most recent call last):
+  File "c:\Users\hp\Documents\Python Codes\OOPS_python\polymorphism.py", line 40, in <module>
+    print(a+b)
+          ~^~
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+
+a = 5
+b = 6
+
+print(a+b)
+
+print(int.__add__(a,b))
 
 #output:
-# Spell chack
-# Convention check
-# Compiling
-# Running
+11
+11
 
-#---------------------------------------------------------------------------------------------------
+a = '5'
+b = '6'
 
-# Operator overloading:
+print(a+b)
 
-# a = 5
-# b = 'World'
-# print(a+b)
+print(str.__add__(a,b))
 
-# #error:
-# Traceback (most recent call last):
-#   File "c:\Users\hp\Documents\Python Codes\OOPS_python\polymorphism.py", line 40, in <module>
-#     print(a+b)
-#           ~^~
-# TypeError: unsupported operand type(s) for +: 'int' and 'str'
+#output:
+56
+56
 
+class Student:
 
-# a = 5
-# b = 6
+    def __init__(self, m1,m2):
+        self.m1=m1
+        self.m2=m2
 
-# print(a+b)
+    def __add__(self,other):
+        m1 = self.m1 + other.m1
+        m2 = self.m2 + other.m2
+        s3 = Student(m1,m2)
 
-# print(int.__add__(a,b))
+        return s3
 
-# #output:
-# 11
-# 11
-
-# a = '5'
-# b = '6'
-
-# print(a+b)
-
-# print(str.__add__(a,b))
-
-# #output:
-# 56
-# 56
-
-# class Student:
-
-#     def __init__(self, m1,m2):
-#         self.m1=m1
-#         self.m2=m2
-
-#     def __add__(self,other):
-#         m1 = self.m1 + other.m1
-#         m2 = self.m2 + other.m2
-#         s3 = Student(m1,m2)
-
-#         return s3
-
-# s1 = Student(58,69)   #  -> Student.__add__(s1,s2)
-# s2 = Student(60,65)
+s1 = Student(58,69)   #  -> Student.__add__(s1,s2)
+s2 = Student(60,65)
 
 
-# s3 = s1+s2
+s3 = s1+s2
 
-# print(s3.m1)
+print(s3.m1)
 
-# #output:
-# 118
+#output:
+118
 
 
-# class Student:
+class Student:
 
-#     def __init__(self, m1,m2):
-#         self.m1=m1
-#         self.m2=m2
+    def __init__(self, m1,m2):
+        self.m1=m1
+        self.m2=m2
 
-#     def __add__(self,other):
-#         m1 = self.m1 + other.m1
-#         m2 = self.m2 + other.m2
-#         s3 = Student(m1,m2)
+    def __add__(self,other):
+        m1 = self.m1 + other.m1
+        m2 = self.m2 + other.m2
+        s3 = Student(m1,m2)
 
-#         return s3
+        return s3
     
-#     def __gt__(self,other):
-#         r1 = self.m1 + self.m2
-#         r2 = other.m1 + other.m2
+    def __gt__(self,other):
+        r1 = self.m1 + self.m2
+        r2 = other.m1 + other.m2
         
-#         if r1 > r2:
-#             return True
-#         else:
-#             return False
+        if r1 > r2:
+            return True
+        else:
+            return False
 
-# s1 = Student(58,69)   #  -> Student.__add__(s1,s2)
-# s2 = Student(60,65)
+s1 = Student(58,69)   #  -> Student.__add__(s1,s2)
+s2 = Student(60,65)
 
 
-# s3 = s1+s2
+s3 = s1+s2
 
-# if s1 > s2:
-#     print("s1 wins")
-# else:
-#     print("s2 wins")
+if s1 > s2:
+    print("s1 wins")
+else:
+    print("s2 wins")
 
-# #output:
-# s1 wins
+#output:
+s1 wins
 
 class Student:
 
@@ -177,13 +177,75 @@ print(s1.__str__())
 
 print(s1)
 
-#output:
-# s1 wins
-# 9
-# <__main__.Student object at 0x0000014CEC12AF60>
+output:
+s1 wins
+9
+<__main__.Student object at 0x0000014CEC12AF60>
+
+output:
+s1 wins
+9
+(58, 69)
+
+#----------------------------------------------------------------------
+
+# method overloading
+class Student:
+
+    def __init__(self,m1,m2):
+        self.m1=m1
+        self.m2=m2
+
+    def sum(self,a=None,b=None,c=None):
+
+        s= 0
+        if a!=None and b!=None and c!=None:
+            s = a+b+c
+        elif a!=None and b!=None:
+            s=a+b
+        else:
+            s=a
+
+        return s
+
+s1 = Student(58,69)
+s2 = Student(69,65)
+
+print(s1.sum(5))
+print(s1.sum(5,9))
+print(s1.sum(5,9,6))
 
 #output:
-# s1 wins
-# 9
-# (58, 69)
+5
+14
+20
 
+#--------------------------------------------------------------------
+#method overriding
+class A:
+
+    def show(self):
+        print("in A show")
+
+class B(A):
+    pass
+
+a1 = B()
+a1.show()
+
+#output:
+in A show
+
+class A:
+
+    def show(self):
+        print("in A show")
+
+class B(A):
+    print("in B show")
+
+a1 = B()
+a1.show()
+
+#output:
+in B show
